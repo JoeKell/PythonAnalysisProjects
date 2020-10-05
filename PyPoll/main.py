3f2da45f7d4fb69a72e2b579453e3a34cdc4599d
Joseph B. Kell <JBKell@co.anoka.mn.us>
2020-10-05 17:30:25 -0500

    Looking good so far. I'll need to look at the rounding and winner.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,7 +7,9 @@ election_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
 #Initializing values for the results
 VoteCount=0
 Winner=""
-ElectionArray=[]
+CandidateArray=[]
+TallyArray=[]
+PercentArray=[]
 
 # Open and read csv
 with open(election_csv) as csv_file:
@@ -18,7 +20,14 @@ with open(election_csv) as csv_file:
     # Read through each row of data after the header
     for row in csv_reader:
         VoteCount+=1
+        if row[2] in CandidateArray:
+            TallyArray[CandidateArray.index(row[2])]+=1
+        else:
+            CandidateArray.append(row[2])
+            TallyArray.append(1)
 
+for Tally in TallyArray:
+    PercentArray.append(round(Tally/VoteCount*100,5))
 
 #Print the results
 # print("Financial Analysis \n-------------------------")
@@ -32,9 +41,8 @@ with open(election_csv) as csv_file:
 results=open("PyPoll/analysis/results.txt","w")
 results.write("Election Results \n-------------------------\n")
 results.write(f"Total Votes: {VoteCount}\n-------------------------\n")
-for Candidate in ElectionArray:
-    results.write(f"{Candidate[0]}: {Candidate[1]}% [{Candidate[2]}]\n")
-
+for i in range(len(CandidateArray)):
+    results.write(f"{CandidateArray[i]}: {PercentArray[i]}% [{TallyArray[i]}]\n")
 results.write("-------------------------\n")
 results.write(f"Winner: {Winner}\n")
 results.write("-------------------------")
