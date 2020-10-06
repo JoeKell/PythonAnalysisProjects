22b4628fd7028fd009bead2be8ac8df0c74ada7f
Joseph B. Kell <JBKell@co.anoka.mn.us>
2020-10-06 14:05:01 -0500

    Almost there

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,9 +1,26 @@
 #All code designed to be run from the PythonAnalysisProjects folder. If you have file path issues, look there.
 import os
+import re
 
-ParagraphIndex=1
+ParagraphIndex=3
 
 #The file the code reads from
 File = open(os.path.join("PyParagraph", "Resources", f"paragraph_{ParagraphIndex}.txt"),"r")
 Paragraph=File.read().replace("\n"," ").replace("  "," ")
-print(Paragraph)
+Sentences=re.split("(?<=[.!?]) +", Paragraph)
+ApproxSCount=len(Sentences)
+AvSLen=0
+Words=[]
+for Sentence in Sentences:
+    AvSLen+=len(Sentence.split(" "))
+    Words+=Sentence.split(" ")
+AvSLen=round(AvSLen/ApproxSCount,1)
+print(Words)
+
+
+#Printing with the desired format
+print("Paragraph Analysis\n----------------------")
+print(f"Approximate Word Count: ")
+print(f"Approximate Sentence Count: {ApproxSCount}")
+print(f"Average Letter Count: ")
+print(f"Average Sentence Length: {AvSLen}")
